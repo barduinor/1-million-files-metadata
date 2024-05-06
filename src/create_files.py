9dd625b84fb8d7eacd64d0b8bca4763883fffd70
Rui Barbosa <barduinor@gmail.com>
2024-05-06 15:14:48 -0400

    generate files from us_pop async

--- src/create_files.py
+++ src/create_files.py
@@ -5,7 +5,7 @@ from gen_sample_data.generate_data import (
     generate_pdf_async,
 )
 import random
-from datetime import date, timedelta
+from datetime import date
 from dateutil.relativedelta import relativedelta
 
 US_500 = "sample-data/us_pop_500.csv"
@@ -23,14 +23,14 @@ async def create_files():
             for d in range(51):  # Months
 
                 customer_data = await generate_customer_data_async(
-                    customer_id=f"{us_pop['Postal'][i]}-{n+1}",
+                    customer_id=f"{us_pop['Postal'][i]}-{n+1+100000}",
                     state=us_pop["State"][i],
                     postal=us_pop["Postal"][i],
                     date=statement_date,
                     num_transactions=random.randint(1, 27),
                 )
                 statement = await generate_pdf_async(customer_data)
-
+                print(statement)
                 # upload file to box
                 # add metadata to file
                 # remove file from local folder
