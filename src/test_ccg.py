09259bec5496f1f410ed4ed9d800349660bbd0c2
Rui Barbosa <barduinor@gmail.com>
2024-05-07 12:20:00 -0400

    added box ccg client

---
+++ src/test_ccg.py
@@ -0,0 +1,27 @@
+"""Test configuration for developer token access"""
+
+import logging
+from box_utils.box_client_ccg import (
+    ConfigCCG,
+    get_ccg_enterprise_client,
+    get_ccg_user_client,
+)
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("box_sdk_gen").setLevel(logging.CRITICAL)
+
+conf = ConfigCCG()
+
+
+def main():
+    client = get_ccg_enterprise_client(conf)
+    me = client.users.get_user_me()
+    print(f"\nHello, I'm {me.name} ({me.login}) [{me.id}]")
+
+    user_client = get_ccg_user_client(conf, conf.ccg_user_id)
+    me = user_client.users.get_user_me()
+    print(f"\nHello, I'm {me.name} ({me.login}) [{me.id}]")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test_ccg_switch.py
@@ -0,0 +1,39 @@
+"""Test configuration for developer token access"""
+
+import logging
+from box_utils.box_client_ccg import ConfigCCG
+from box_sdk_gen import BoxClient
+from box_sdk_gen import BoxCCGAuth, CCGConfig
+from box_sdk_gen import FileWithInMemoryCacheTokenStorage
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("box_sdk_gen").setLevel(logging.CRITICAL)
+
+
+def main():
+    config = ConfigCCG()
+    ccg = CCGConfig(
+        client_id=config.client_id,
+        client_secret=config.client_secret,
+        enterprise_id=config.enterprise_id,
+        token_storage=FileWithInMemoryCacheTokenStorage(".ent" + config.cache_file),
+    )
+
+    auth = BoxCCGAuth(ccg)
+    client = BoxClient(auth)
+    me = client.users.get_user_me()
+    print(f"\nHello, I'm {me.name} ({me.login}) [{me.id}]")
+
+    auth = auth.with_user_subject(config.ccg_user_id)
+    client = BoxClient(auth)
+    me = client.users.get_user_me()
+    print(f"\nHello, I'm {me.name} ({me.login}) [{me.id}]")
+
+    auth = auth.with_enterprise_subject(config.enterprise_id)
+    client = BoxClient(auth)
+    me = client.users.get_user_me()
+    print(f"\nHello, I'm back to {me.name} ({me.login}) [{me.id}]")
+
+
+if __name__ == "__main__":
+    main()
